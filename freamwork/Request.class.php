b197b554f8c8b39629213b7e7cf3230b5e5bec2a
wgl <1132083961@qq.com>
2019-01-17 12:04:35 +0800

    模板引擎与函数修改

--- freamwork/Request.class.php
+++ freamwork/Request.class.php
@@ -12,12 +12,6 @@ class Request {
 
     /**
      * 获取输入参数 支持过滤和默认值
-     * 使用方法:
-     * <code>
-     * I('id',0); 获取id参数 自动判断get或者post
-     * I('post.name','','htmlspecialchars'); 获取$_POST['name']
-     * I('get.'); 获取$_GET
-     * </code>
      * @param string $name 变量的名称 支持指定类型
      * @param mixed $default 不存在的时候默认值
      * @param mixed $filter 参数过滤方法
