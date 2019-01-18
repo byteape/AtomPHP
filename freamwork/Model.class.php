18573fbce7799dca93cfd1eab5cf3af231910687
wgl <1132083961@qq.com>
2019-01-18 13:49:11 +0800

    模型修改

--- freamwork/Model.class.php
+++ freamwork/Model.class.php
@@ -11,6 +11,8 @@ use Medoo\Medoo;
  */
 class Model {
 
+    static $db = [];
+
     /**
      * 获取连接句柄
      * @param string $configFile
@@ -19,8 +21,13 @@ class Model {
     public static function db($configFile = '') {
         $configFile = $configFile ? $configFile : 'database';
         $dbconfig   = require __DIR__ . "/../config/" . $configFile . '.php';
-        $database   = new Medoo($dbconfig);
-        return $database;
+        if (self::$db) {
+            return self::$db;
+        } else {
+            $database = new Medoo($dbconfig);
+            self::$db = $database;
+            return $database;
+        }
     }
 
 }
